863929889883ed00d05dc32f4c6be434830c89e7
JoaoLucasRB <jlgf192013@gmail.com>
2023-06-07 13:14:55 -0300

    feat: add fetch and set to testimony list

--- react/components/Testimony/index.jsx
+++ react/components/Testimony/index.jsx
@@ -1,7 +1,31 @@
 //react/Title.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Testimony() {
+  const [testimonies, setTestimonies] = useState([])
+
+  /**
+   * Fetch the list of testimonies from MasterData and set the list to state "testimonies".
+   *
+   */
+  function fetchTestimonies () {
+    fetch("/api/dataentities/DJ/search?_fields=name,city,testimony,date", {
+      headers: {
+		    "Accept": "application/json",
+		    "Content-Type": "application/json",
+        "REST-Range": "resources=0-10",
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        setTestimonies(res);
+      })
+  }
+
+  useEffect(() => {
+    fetchTestimonies()
+  }, [])
+
   return (
     <div>
       Testimony
