ac8acea74d02eca1d33b10ceb2b80873a0dcc874
JoaoLucasRB <jlgf192013@gmail.com>
2023-06-07 15:14:19 -0300

    feat: final structure and style for the testimony block

--- react/components/Testimony/index.jsx
+++ react/components/Testimony/index.jsx
@@ -1,5 +1,6 @@
 //react/Title.tsx
 import React, { useEffect, useState } from 'react';
+import styles from './styles.css'
 
 function Testimony({
   SliderLayout
@@ -11,7 +12,7 @@ function Testimony({
    *
    */
   function fetchTestimonies (props) {
-    fetch("/api/dataentities/DJ/search?_fields=name,city,testimony,date", {
+    fetch("/api/dataentities/DJ/search?_fields=id,picture,name,city,testimony,date", {
       headers: {
 		    "Accept": "application/json",
 		    "Content-Type": "application/json",
@@ -20,8 +21,29 @@ function Testimony({
     })
       .then(res => res.json())
       .then(res => {
+        console.log('res', res)
         setTestimonies(res);
       })
+  }
+
+  /**
+   * Formats the received date from Masterdata to the format DD/MM/YYYY.
+   *
+   * @param {string} date The date to format.
+   */
+  function formatDate(date) {
+    let objectDate = new Date(date);
+    objectDate.setTime(objectDate.getTime() + (3*60*60*1000))
+    let day = objectDate.getDate();
+    let month = objectDate.getMonth() + 1;
+    const year = objectDate.getFullYear();
+
+    day = day < 10 ? `0${day}`: day;
+    month = month < 10 ? `0${month}`: month;
+
+    console.log('data', day, month, year)
+
+    return `${day}/${month}/${year}`;
   }
 
   useEffect(() => {
@@ -31,11 +53,28 @@ function Testimony({
   return (
     <SliderLayout>
       {testimonies.map(testimony => 
-        <div>
-          {testimony.name}
-          {testimony.testimony}
-          {testimony.city}
-          {testimony.date}
+        <div className={styles.wrapper}>
+          <div className={styles.container}>
+            {testimony.picture ?
+              <div className={styles.picture}>
+                <img src={
+                  `/api/dataentities/DJ/documents/${testimony.id}/picture/attachments/${testimony.picture}`
+                } alt="customer-picture" />
+              </div>
+            : <></>}
+            <div className={styles.name}>
+              {testimony.name}
+            </div>
+            <div className={styles.testimony}>
+              {testimony.testimony}
+            </div>
+            <div className={styles.city}>
+              {testimony.city}
+            </div>
+            <div className={styles.date}>
+              {formatDate(testimony.date)}
+            </div>
+          </div>
         </div>
       )}
     </SliderLayout>
